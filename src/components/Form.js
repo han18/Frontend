276922f61138a1437ceec769da2046dfbe435684
han18 <han.fadah@gmail.com>
2024-02-08 13:39:17 -0500

    added more detials to the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,16 +5,34 @@ function Form() {
   return (
     <div>
       <form>
+        <label for="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          minlength="3"
+          maxlength="15"
+          required
+        />
+
         <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" required />
+        <input type="text" id="firstName" name="first_name" required />
 
         <label for="lastName">Last Name:</label>
-        <input type="text" id="firstName" name="firstName" required />
+        <input type="text" id="lastName" name="last_name" required />
 
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required />
+
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          minlength="8"
+          maxlength="50"
+          required
+        />
 
         <button type="submit">Submit</button>
       </form>
