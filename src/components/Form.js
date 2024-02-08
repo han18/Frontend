25f671b96c434f1081cbed1d105871cf47a38b33
han18 <han.fadah@gmail.com>
2024-02-08 13:05:58 -0500

    added the email and password input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Form() {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form>
+        <label for="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" required />
+
+        <label for="lastName">Last Name:</label>
+        <input type="text" id="firstName" name="firstName" required />
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </form>
+    </div>
+  );
 }
 
 export default Form;
