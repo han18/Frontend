ca56a24ca739ad190dcd1aff0006f79f1a6bee16
han18 <han.fadah@gmail.com>
2024-02-08 18:34:09 -0500

    structured the form validation

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Getting Started with Create React App
 
-/**********************************************\*\*\*\***********************************************
+imported the react-router-dom
+
+**********************\*\***********************\*\*\*\***********************\*\***********************
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,44 +1,75 @@
 // this file is the form file from that reflect the users collection schema
 import "../styles/button.css";
 
-function Form() {
+import React, { useState } from "react";
+
+const Form = () => {
+  const [formData, setFormData] = useState({});
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Create an account</h1>
         <label htmlFor="username">Username:</label>
         <input
           type="text"
           id="username"
           name="username"
-          minlength="3"
-          maxlength="15"
+          minLength="3"
+          maxLength="15"
           required
+          value={formData.username}
+          onChange={handleChange}
         />
 
         <label htmlFor="firstName">First Name:</label>
-        <input type="text" id="firstName" name="first_name" required />
+        <input
+          type="text"
+          id="firstName"
+          name="first_name"
+          required
+          value={formData.first_name}
+          onChange={handleChange}
+        />
 
         <label htmlFor="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="last_name" required />
+        <input
+          type="text"
+          id="lastName"
+          name="last_name"
+          required
+          value={formData.last_name}
+          onChange={handleChange}
+        />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          value={formData.email}
+          onChange={handleChange}
+        />
 
         <label htmlFor="password">Password:</label>
         <input
           type="password"
           id="password"
           name="password"
-          minlength="8"
-          maxlength="50"
+          minLength="8"
+          maxLength="50"
           required
+          value={formData.password}
+          onChange={handleChange}
         />
 
-        <button type="submit">Submit</button>
+        <button onClick={setFormData} type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default Form;
