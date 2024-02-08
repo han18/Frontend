774ebb9f6f720ac151a4e3297bcf01f927ded159
han18 <han.fadah@gmail.com>
2024-02-08 13:07:31 -0500

    created the email

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,6 +14,8 @@ function Form() {
         <input type="email" id="email" name="email" required />
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required />
+
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
