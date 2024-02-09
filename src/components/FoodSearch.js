d6d33c29fe5ea24ba1c0fa7cf1fca24059ad2290
han18 <han.fadah@gmail.com>
2024-02-09 14:27:34 -0500

    handling inputs

--- src/components/FoodSearch.js
+++ src/components/FoodSearch.js
@@ -1,6 +1,16 @@
-import React from "react";
+// this file is where the food search component is rendered
+import { useState } from "react";
 
 function FoodSearch() {
+  // State to hold the data of our form
+  const [formData, setFormData] = useState({
+    searchterm: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return <div>FoodSearch</div>;
 }
 
