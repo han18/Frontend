ee9698cab46f8b18f9f05233ec329b7d7d3d9549
han18 <han.fadah@gmail.com>
2024-02-09 12:11:22 -0500

    adding the useEffect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import Form from "./components/Form";
 
 function App() {
+  const [recipes, setRecipes] = useState(null);
+
   return (
     <div className="App">
       <h1>Howedible</h1>
