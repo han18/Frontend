6c4cffbf7acaa792719ba8bf33b446eb6823098f
han18 <han.fadah@gmail.com>
2024-02-10 09:32:31 -0500

    added the useeffect and linked the api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import "./styles/FoodForm.module.css";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchTerm}`
+        );
+        const data = await response.json();
+        setSearchResults(data.meals || []);
+        console.log(data);
+      } catch (error) {
+        console.error("Error");
+      }
+    };
+  }, []);
 };
 
 export default App;
