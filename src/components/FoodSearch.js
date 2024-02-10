30df4ed3d642a1a08e254647e7473c282997af01
han18 <han.fadah@gmail.com>
2024-02-10 09:55:51 -0500

    created the search input for search input

--- src/components/FoodSearch.js
+++ src/components/FoodSearch.js
@@ -1,7 +1,21 @@
 const FoodSearch = ({ setSearchTerm }) => {
+  // to handle the search input change
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  return (
+    <div className="container">
+      <label htmlFor="searchInput">Search:</label>
+      <input
+        className="items"
+        type="text"
+        id="searchInput"
+        onChange={handleInputChange}
+        placeholder="Enter a letter search query"
+      />
+    </div>
+  );
 };
 
 export default FoodSearch;
