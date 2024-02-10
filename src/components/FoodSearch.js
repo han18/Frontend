d8b48e1537406032107936830c67792cce4edf15
han18 <han.fadah@gmail.com>
2024-02-10 09:48:41 -0500

    creating the search compoment

--- src/components/FoodSearch.js
+++ src/components/FoodSearch.js
@@ -1,22 +1,3 @@
-import React from "react";
-
-const FoodSearch = ({ setSearchTerm }) => {
-  const handleInputChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  return (
-    <div className="container">
-      <label htmlFor="searchInput">Search:</label>
-      <input
-        className="items"
-        type="text"
-        id="searchInput"
-        onChange={handleInputChange}
-        placeholder="Enter search query"
-      />
-    </div>
-  );
-};
+const FoodSearch = ({ setSearchTerm }) => {};
 
 export default FoodSearch;
