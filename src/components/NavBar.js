dfa514b8a429716752cda615cefb2f016e535b70
han18 <han.fadah@gmail.com>
2024-02-12 13:08:25 -0500

    re fixing the form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,14 +11,44 @@ const NavBar = ({ user, setUser }) => {
     localStorage.removeItem("user");
     navigate("/");
   };
-      
-        <h1>HowEdible</h1>
+  return (
+    <nav style={{ backgroundColor: "#333", padding: "10px", color: "white" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>
+          <Link style={{ color: "white", textDecoration: "none" }} to="/">
+            HowEdible
+          </Link>
+        </h1>
         <ul style={{ listStyle: "none", display: "flex", gap: "20px" }}>
           <li>
-            <Link to="/form">
-              <a href="#">Sign up</a>
-            </Link>
+            <Link to="/">Home</Link>
           </li>
+          {user?.email ? (
+            <li>
+              <span style={{ color: "black" }} onClick={logOut}>
+                <a>Log out</a>
+              </span>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link to="/form">
+                  <a href="#">Sign up</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  <a href="#">Login</a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
