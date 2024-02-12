df56376a84b35b6a6ee04b21f4474e279ffc0239
han18 <han.fadah@gmail.com>
2024-02-12 13:01:52 -0500

    correcting the navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import React from "react";
 // import "../styles/Navbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-const NavBar = () => {
-  return (
-    <nav style={{ backgroundColor: "#333", padding: "10px", color: "white" }}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+const NavBar = ({ user, setUser }) => {
+  const navigate = useNavigate();
+
+  // log out handler
+  const logOut = () => {
+    setUser({});
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+      
         <h1>HowEdible</h1>
         <ul style={{ listStyle: "none", display: "flex", gap: "20px" }}>
           <li>
