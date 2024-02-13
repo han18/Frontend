7f157153299bdf826113aadd53e573039eef7291
han18 <han.fadah@gmail.com>
2024-02-13 14:25:15 -0500

    updating some comments and css

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -4,14 +4,17 @@ import { useNavigate } from "react-router-dom";
 const Profile = ({ user }) => {
   const navigate = useNavigate();
 
-  // if user is not logged in, redirect them to the home page
+  // if user is not logged in, redirect them to the home page.. if user is logged in display the welcoming message
   if (!user?.email) {
     return navigate("/");
   }
 
   return (
     <main>
-      <div className="container" style={{ margin: "50px" }}>
+      <div
+        className="container"
+        style={{ margin: "80px", color: "blue", fontSize: "20px" }}
+      >
         <h1>Welcome, {user.username}</h1>
         <p>First Name: {user.first_name}</p>
         <p>Last Name: {user.last_name}</p>
